fb63606202df26fcc82f58b6cfb7bac82ce64e77
hussu <hussu010@gmail.com>
2023-06-17 08:25:15 +0545

    fix: broadcast artworks route

--- backend/index.ts
+++ backend/index.ts
@@ -9,12 +9,16 @@ import cors from "cors";
 
 import { errorLogger, errorResponder } from "./src/common/middlewares/errors";
 
+import artworksRoute from "./src/artworks/artworks.route";
+
 app.enable("trust proxy");
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use(errorLogger);
+
+app.use("/artworks", artworksRoute);
 
 app.use(errorResponder);
 
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,6 @@
 import app from "./index";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4200;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
